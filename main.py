872941b616ed9ecd149c7010a9fd5f9ef1037cb4
Gabriel Hoffman Silva <30511353+Hoffmano@users.noreply.github.com>
2020-11-01 21:18:00 -0300

    feat: tentativa de deploy

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import os
 from flask import Flask
 from flask_cors import CORS
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
 
-cors = CORS(app, resource={r"/*": {"origins": "*"}})
+# cors = CORS(app, resource={r"/*": {"origins": "*"}})
+
+api = Api(app)
 
 
-@app.route("/", methods=['GET'])
-def index():
-    return "<h1>Hello World!</h1>"
+class Main(Resource):
+    def get(self):
+        return {"name": "api"}
 
 
-@app.route("/deploy", methods=['GET'])
-def deploy():
-    return "<h1>Testando deploy GitHub x Heroku </h1>"
+# @ app.route("/", methods=['GET'])
+# def index():
+#     return "<h1>Testando deploy GitHub x Heroku </h1>"
 
 
 def main():
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port, debug=True)
 
+
+api.add_resource(Main, "/")
 
 if __name__ == "__main__":
     main()
