f7581e8a8adfbd7c379b5cd0309b803aaf46cb16
Gabriel Hoffman Silva <30511353+Hoffmano@users.noreply.github.com>
2020-11-01 21:37:40 -0300

    fix: tenta concertar deploy no heroku

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
-from flask_restful import Api, Resource
 
 app = Flask(__name__)
-api = Api(app)
 CORS(app, resource={r"/*": {"origins": "*"}})
 
 
-class Main(Resource):
-    def get(self):
-        return {"name": "api"}
-
-
-# @ app.route("/", methods=['GET'])
-# def index():
-#     return "<h1>Testando deploy GitHub x Heroku </h1>"
+@app.route("/", methods=["GET"])
+def get():
+    return jsonify(name="hello world")
 
 
 def main():
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
-
-
-api.add_resource(Main, "/")
 
 if __name__ == "__main__":
     main()
