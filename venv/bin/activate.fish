4bac7c54cb1a8de2cf6acacdb9f931528a9f2eed
Gabriel Hoffman <gabriel.silva@amparosaude.com>
2020-12-20 11:22:58 -0300

    configuring-suricator

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/hoffman/Documents/code/GitHub/nlp-lyrics-api/venv"
+set -gx VIRTUAL_ENV "/home/hoffman/Documents/Github/suricator-nlp/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
